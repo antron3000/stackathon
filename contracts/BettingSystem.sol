0f7f89772142347467c519aeeaf53e492236170f
Antoine De Vuyst <fortyonepointsix@gmail.com>
2018-11-25 08:42:40 -0500

    working dapp

--- contracts/BettingSystem.sol
+++ contracts/BettingSystem.sol
@@ -1,20 +1,28 @@
 pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
+//todo calculate payouts properly
+//re-bet
+//display json data
+
 contract BettingSystem {
     struct Match {
         mapping(address => int) positions;
-        address[] bettorList;
-        uint totalAmount;
+        uint betsfor;
+        mapping(bool=>uint) numBets;
         bool finalized;
+        uint totalAmount;
     }
 
-    mapping(bytes32 => Match) bets;
+    mapping(bytes32 => Match) public bets;
 
 
     function bet(bytes32 matchId, bool _for) payable public {
+        require(msg.value >0);
+        require(bets[matchId].positions[msg.sender]==0);
+        require(bets[matchId].finalized==false);
         bets[matchId].positions[msg.sender] = int(_for ? msg.value : -msg.value);
-        bets[matchId].bettorList.push(msg.sender);
+        bets[matchId].numBets[_for]+=1;
         bets[matchId].totalAmount += msg.value;
     }
 
@@ -22,7 +30,9 @@ contract BettingSystem {
         require(finalPrice <= 100);
         require(sigs[0].length == graders.length, "insufficient signatures passed in");
 
+
         bytes32 matchId = keccak256(abi.encodePacked(witness, graderQuorum, graders[0], graders[1], graders[2])); // figure out ABI encoding
+        require(bets[matchId].positions[msg.sender]!= 0);
         bytes32 messageHash = keccak256(abi.encodePacked(matchId, finalPrice)); // FIXME: should use contract addr to prevent replay attacks
         bytes32 messageHash2 = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
 
@@ -40,11 +50,17 @@ contract BettingSystem {
         }
 
         require(validated >= graderQuorum, "insufficient graders for quorum");
-
-        //uint toWithdraw = bets[matchId].totalAmount / bets[matchId].bettorList.length; // FIXME: invalid opcode
+        uint toWithdraw;
+        if(bets[matchId].positions[msg.sender]> 0){
+             toWithdraw = (finalPrice*bets[matchId].totalAmount / bets[matchId].numBets[true])/100;
+        } else {
+             toWithdraw = ((100-finalPrice)*bets[matchId].totalAmount / bets[matchId].numBets[false])/100;
+        }
         bets[matchId].positions[msg.sender] = 0;
-        msg.sender.transfer(0);
+        msg.sender.transfer(toWithdraw);
     }
+
+
 
     function recoverFunds(uint256 graderQuorum, address[] memory graders, bytes32 detailsHash, uint recoveryTime, uint8 cancelPrice) public {
         bytes32 witness = keccak256(abi.encodePacked(detailsHash, recoveryTime, cancelPrice));

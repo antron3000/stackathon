01aabf397f114f34cdffd4ff1b6d74ab0625f615
Doug Hoyte <doug@hcsw.org>
2018-11-24 22:58:27 -0500

    more stuff

--- contracts/Interface.sol
+++ contracts/Interface.sol
@@ -1,25 +1,29 @@
 pragma solidity ^0.5.0;
-//pragma experimental ABIEncoderV2;
+pragma experimental ABIEncoderV2;
 
 
 contract BettingSystem {
-    function claim(uint witness, uint graderQuorum, address[] memory graders, uint8 finalPrice, bytes32[][3] memory sigs) public {
+    mapping(bytes32 => bool) claimed;
+
+    function claim(bytes32 witness, uint256 graderQuorum, address[] memory graders, uint8 finalPrice, bytes32[][3] memory sigs) public {
         require(finalPrice <= 100);
 
-        uint matchId = keccak256(abi.encodePacked(witness, graderQuorum, graders));
-        uint messageHash = keccak256(abi.encodePacked(this, matchId, finalPrice));
-        uint messageHash2 = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
+        bytes32 matchId = keccak256(abi.encodePacked(witness, graderQuorum, graders));
+        bytes32 messageHash = keccak256(abi.encodePacked(this, matchId, finalPrice));
+        bytes32 messageHash2 = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
 
-        uint validated = 0;
+        uint256 validated = 0;
 
         for (uint i = 0; i < graders.length; i++) {
             if (sigs[i][0] != 0) {
-                address signer = ecrecover(messageHash2, sigs[i][2], sigs[i][0], sigs[i][1]);
+                address signer = ecrecover(messageHash2, uint8(sigs[i][2][0]), sigs[i][0], sigs[i][1]);
                 require(signer == graders[i], "bad grader signature");
                 validated++;
             }
         }
 
         require(validated >= graderQuorum, "insufficient graders for quorum");
+
+        claimed[matchId] = true;
     }
 }

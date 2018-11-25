2d7d7b66e53ffd0a156ce22af9a317be4a4e0a3d
Doug Hoyte <doug@hcsw.org>
2018-11-25 04:18:45 -0500

    Revert "a"
    This reverts commit 06f1fbf8daaa3d31b50ecf4d32a02d2df4dafb6f.

--- contracts/BettingSystem.sol
+++ contracts/BettingSystem.sol
@@ -3,29 +3,27 @@ pragma experimental ABIEncoderV2;
 
 
 contract BettingSystem {
-    struct Match {
+    struct Bet {
         mapping(address => int) positions;
         address[] bettorList;
         uint totalAmount;
         bool finalized;
     }
 
-    mapping(bytes32 => Match) matches;
-
+    mapping(bytes32 => Bet) bets;
 
 
     function bet(bytes32 matchId, bool _for) payable public {
-
-
-        matches[matchId].positions[msg.sender] = int(_for ? msg.value : -msg.value);
-        matches[matchId].bettorList.push(msg.sender);
-        matches[matchId].totalAmount += msg.value;
+        bets[matchId].positions[msg.sender] = int(_for ? msg.value : -msg.value);
+        bets[matchId].bettorList.push(msg.sender);
+        bets[matchId].totalAmount += msg.value;
     }
 
-    function claim(bytes32 witness, uint256 graderQuorum, address[] memory graders, uint8 finalPrice, bytes32[][] memory sigs) public {
+    function claim(bytes32 witness, uint256 graderQuorum, address[] memory graders, uint8 finalPrice, bytes32[3][] memory sigs) public {
         require(finalPrice <= 100);
+        require(sigs[0].length == graders.length, "insufficient signatures passed in");
 
-        bytes32 matchId = keccak256(abi.encodePacked(witness, graderQuorum, graders));
+        bytes32 matchId = keccak256(abi.encodePacked(witness, graderQuorum, graders[0], graders[1], graders[2])); // figure out ABI encoding
         bytes32 messageHash = keccak256(abi.encodePacked(matchId, finalPrice)); // FIXME: should use contract addr to prevent replay attacks
         bytes32 messageHash2 = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
 
@@ -33,7 +31,10 @@ contract BettingSystem {
 
         for (uint i = 0; i < graders.length; i++) {
             if (sigs[i][0] != 0) {
-                address signer = ecrecover(messageHash2, uint8(sigs[i][2][0]), sigs[i][0], sigs[i][1]);
+                bytes32 r = sigs[i][0];
+                bytes32 s = sigs[i][1];
+                uint8 v = uint8(sigs[i][2][31]);
+                address signer = ecrecover(messageHash2, v, r, s);
                 require(signer == graders[i], "bad grader signature");
                 validated++;
             }
@@ -41,9 +42,9 @@ contract BettingSystem {
 
         require(validated >= graderQuorum, "insufficient graders for quorum");
 
-        uint toWithdraw = matches[matchId].totalAmount/matches[matchId].bettorList.length;
-        matches[matchId].positions[msg.sender] = 0;
-        msg.sender.transfer(toWithdraw);
+        //uint toWithdraw = bets[matchId].totalAmount / bets[matchId].bettorList.length; // FIXME: invalid opcode
+        bets[matchId].positions[msg.sender] = 0;
+        msg.sender.transfer(0);
     }
 
     function recoverFunds(uint256 graderQuorum, address[] memory graders, bytes32 detailsHash, uint recoveryTime, uint8 cancelPrice) public {
@@ -53,6 +54,6 @@ contract BettingSystem {
         require(recoveryTime < block.timestamp);
 
         // finalize matchId at cancelPrice
-        matches[matchId].finalized = true;
+        bets[matchId].finalized = true;
     }
 }
